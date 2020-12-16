ded3ff517f4ed367e9a693c0743c4bc490decfeb
Kuba Szostko <kubaszostko@gmail.com>
2020-12-16 09:59:36 +0100

    created Product class

---
+++ app/src/main/java/com/example/ecommerce/Product.java
@@ -0,0 +1,64 @@
+package com.example.ecommerce;
+
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "product")
+public class Product {
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+    private String name;
+    private double price;
+    private String image;
+    private String description;
+    // @TODO: create Review class
+    // private List<Review> reviews;
+
+
+    public Product(String name, double price, String image, String description) {
+        this.name = name;
+        this.price = price;
+        this.image = image;
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}

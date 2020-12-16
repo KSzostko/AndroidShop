95d110b0aeeb7dfe12043483eeba9913590d7470
Kuba Szostko <kubaszostko@gmail.com>
2020-12-16 12:57:21 +0100

    created OrderItemDao interface

---
+++ app/src/main/java/com/example/ecommerce/OrderItemDao.java
@@ -0,0 +1,29 @@
+package com.example.ecommerce;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import java.util.List;
+
+@Dao
+public interface OrderItemDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insert(OrderItem orderItem);
+
+    @Update
+    void update(OrderItem orderItem);
+
+    @Delete
+    void delete(OrderItem orderItem);
+
+    @Query("SELECT * FROM orderItem")
+    LiveData<List<OrderItem>> findAll();
+
+    @Query("SELECT * FROM orderItem WHERE productId=:productId")
+    LiveData<List<OrderItem>> findOrderItemsForProduct(int productId);
+}

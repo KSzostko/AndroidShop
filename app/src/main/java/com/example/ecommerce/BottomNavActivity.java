ec66a6e7b545435adbb2b91859b42fe8682ea58a
Kuba Szostko <kubaszostko@gmail.com>
2020-12-27 18:10:57 +0100

    created BottomNavActivity

---
+++ app/src/main/java/com/example/ecommerce/BottomNavActivity.java
@@ -0,0 +1,53 @@
+package com.example.ecommerce;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public abstract class BottomNavActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch(item.getItemId()) {
+                    case R.id.action_home:
+                        Toast.makeText(BottomNavActivity.this, "Home", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.action_search:
+                        Toast.makeText(BottomNavActivity.this, "Search", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.action_order:
+                        Toast.makeText(BottomNavActivity.this, "Order", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+
+                return true;
+            }
+        });
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        Fragment fragment = fragmentManager.findFragmentById(R.id.fragment_container);
+
+        if(fragment == null) {
+            fragment = createFragment();
+            fragmentManager.beginTransaction()
+                    .add(R.id.fragment_container, fragment)
+                    .commit();
+        }
+    }
+
+    protected abstract Fragment createFragment();
+}

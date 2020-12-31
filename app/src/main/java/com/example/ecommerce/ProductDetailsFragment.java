4261bfa3ca06d77aa5ab98b745f52ced9cd59a01
Kuba Szostko <kubaszostko@gmail.com>
2020-12-31 20:43:11 +0100

    added currency spinner to the product details frgament

--- app/src/main/java/com/example/ecommerce/ProductDetailsFragment.java
+++ app/src/main/java/com/example/ecommerce/ProductDetailsFragment.java
@@ -7,9 +7,13 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.squareup.picasso.Picasso;
@@ -19,7 +23,7 @@ import com.squareup.picasso.Picasso;
  * Use the {@link ProductDetailsFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class ProductDetailsFragment extends Fragment {
+public class ProductDetailsFragment extends Fragment implements AdapterView.OnItemSelectedListener {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -30,6 +34,7 @@ public class ProductDetailsFragment extends Fragment {
     private ImageView mProductImageView;
     private TextView mProductNameView;
     private TextView mProductPriceView;
+    private Spinner mCurrencySpinner;
     private FloatingActionButton mBuyButtonView;
 
     public ProductDetailsFragment() {
@@ -71,6 +76,7 @@ public class ProductDetailsFragment extends Fragment {
         mProductImageView = view.findViewById(R.id.details_image);
         mProductNameView = view.findViewById(R.id.details_name);
         mProductPriceView = view.findViewById(R.id.details_price);
+        mCurrencySpinner = view.findViewById(R.id.currency_spinner);
         mBuyButtonView = view.findViewById(R.id.fab_buy);
 
         String testImgUrl = "https://www.nvidia.com/content/dam/en-zz/Solutions/geforce/ampere/rtx-3090/geforce-rtx-3090-shop-630-d@2x.png";
@@ -79,8 +85,31 @@ public class ProductDetailsFragment extends Fragment {
                 .load(testImgUrl)
                 .into(mProductImageView);
 
+        // spinner setup
+        if(mCurrencySpinner != null) {
+            mCurrencySpinner.setOnItemSelectedListener(this);
+        }
+
+        // TODO: Maybe try to style it
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getContext(), R.array.spinner_labels, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        if(mCurrencySpinner != null) {
+            mCurrencySpinner.setAdapter(adapter);
+        }
+
         // TODO: add fab button listener
 
         return view;
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String spinnerLabel = parent.getItemAtPosition(position).toString();
+        Toast.makeText(getContext(), spinnerLabel, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
     }
 }

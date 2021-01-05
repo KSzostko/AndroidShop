56b2cd21548cbf2d0f32ee4ac4ad15d7bb9c2e65
Kuba Szostko <kubaszostko@gmail.com>
2021-01-05 17:02:31 +0100

    added more content and styled main fragment

--- app/src/main/java/com/example/ecommerce/MainFragment.java
+++ app/src/main/java/com/example/ecommerce/MainFragment.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.RatingBar;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,7 +14,11 @@ import androidx.fragment.app.Fragment;
 import com.squareup.picasso.Picasso;
 
 public class MainFragment extends Fragment {
-    private ImageView mImageView;
+    private ImageView recommendedImageView;
+    private RatingBar recommendedRating;
+
+    private ImageView bestsellerImageView;
+    private RatingBar bestsellserRating;
 
     public MainFragment() {
 
@@ -28,13 +33,24 @@ public class MainFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_main, container, false);
 
-        mImageView = view.findViewById(R.id.main_image);
+        recommendedImageView = view.findViewById(R.id.recommended_image);
+        recommendedRating = view.findViewById(R.id.recommended_rating);
+
+        bestsellerImageView = view.findViewById(R.id.bestseller_image);
+        bestsellserRating = view.findViewById(R.id.bestseller_rating);
+
+        recommendedRating.setRating(5);
 
         String testImgUrl = "https://www.nvidia.com/content/dam/en-zz/Solutions/geforce/ampere/rtx-3090/geforce-rtx-3090-shop-630-d@2x.png";
         // loading img
         Picasso.get()
                 .load(testImgUrl)
-                .into(mImageView);
+                .into(recommendedImageView);
+
+        bestsellserRating.setRating(4);
+        Picasso.get()
+                .load(testImgUrl)
+                .into(bestsellerImageView);
 
         return view;
     }

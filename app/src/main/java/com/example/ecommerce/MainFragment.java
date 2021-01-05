2c5f9f7b5577776e3131c5d5720863f733566bfb
Kuba Szostko <kubaszostko@gmail.com>
2021-01-05 11:27:53 +0100

    added some styles to the main fragment

--- app/src/main/java/com/example/ecommerce/MainFragment.java
+++ app/src/main/java/com/example/ecommerce/MainFragment.java
@@ -4,12 +4,16 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.squareup.picasso.Picasso;
+
 public class MainFragment extends Fragment {
+    private ImageView mImageView;
 
     public MainFragment() {
 
@@ -23,6 +27,14 @@ public class MainFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_main, container, false);
+
+        mImageView = view.findViewById(R.id.main_image);
+
+        String testImgUrl = "https://www.nvidia.com/content/dam/en-zz/Solutions/geforce/ampere/rtx-3090/geforce-rtx-3090-shop-630-d@2x.png";
+        // loading img
+        Picasso.get()
+                .load(testImgUrl)
+                .into(mImageView);
 
         return view;
     }

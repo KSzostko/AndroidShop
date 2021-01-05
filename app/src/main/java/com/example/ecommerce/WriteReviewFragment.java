1263cd8845c48f15e9ddd3b8dda936e7cc25ee7e
Kuba Szostko <kubaszostko@gmail.com>
2021-01-05 22:38:40 +0100

    added content to review tab

--- app/src/main/java/com/example/ecommerce/WriteReviewFragment.java
+++ app/src/main/java/com/example/ecommerce/WriteReviewFragment.java
@@ -1,20 +1,32 @@
 package com.example.ecommerce;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RatingBar;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class WriteReviewFragment extends Fragment {
+    private RatingBar ratingBar;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_write_review_tab, container, false);
+
+        ratingBar = (RatingBar) view.findViewById(R.id.write_review_rating);
+
+        ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+                Log.i("WriteReviewFragment", "Siema");
+            }
+        });
 
         return view;
     }

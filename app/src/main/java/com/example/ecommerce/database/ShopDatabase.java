ba5f3644e295bedadf8beeb8e7b58da80e404abd
Kuba Szostko <kubaszostko@gmail.com>
2021-01-06 15:13:45 +0100

    prepopulated db with products

--- app/src/main/java/com/example/ecommerce/database/ShopDatabase.java
+++ app/src/main/java/com/example/ecommerce/database/ShopDatabase.java
@@ -2,9 +2,11 @@ package com.example.ecommerce.database;
 
 import android.content.Context;
 
+import androidx.annotation.NonNull;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -20,12 +22,40 @@ public abstract class ShopDatabase extends RoomDatabase {
     public static final int NUMBER_OF_THREADS = 4;
     public static final ExecutorService databaseWriteExecutor = Executors.newFixedThreadPool(NUMBER_OF_THREADS);
 
+    // prepopulating db with products
+    private static RoomDatabase.Callback sRoomDatabaseCallback = new RoomDatabase.Callback() {
+        @Override
+        public void onOpen(@NonNull SupportSQLiteDatabase db) {
+            super.onOpen(db);
+            databaseWriteExecutor.execute(() -> {
+                ProductDao dao = INSTANCE.productDao();
+                // deleteAll probably should be removed later
+                dao.deleteAll();
+
+                // TODO: generate separate method for this
+                Product product1 = new Product("MSI GeForce RTX 3090 GAMING X TRIO 24GB GDDR6X", 8080.00,
+                        "https://www.nvidia.com/content/dam/en-zz/Solutions/geforce/ampere/rtx-3090/geforce-rtx-3090-shop-300-t.png",
+                        "The latest iteration of MSI’s iconic GAMING series once again brings performance, low-noise efficiency, and aesthetics that hardcore" +
+                                " gamers have come to recognize and trust. " + "Now you too can enjoy all your favorite games with a powerful graphics card " +
+                                "that stays cool and silent. Just the way you like it.");
+                Product product2 = new Product("Huawei Matebook X Pro", 6999.00,
+                        "https://www.nowanet.sklep.pl/wp-content/uploads/2019/05/Huawei-MateBook-X-Pro-Laptop-Intel-Core-i5-8250U-8GB-256GB-Gray-667502-.jpg",
+                        "With an industry-leading processor and 3K FullView Display, HUAWEI MateBook X Pro takes your experience beyond the extraordinary. ");
+
+                dao.insert(product1);
+                dao.insert(product2);
+            });
+        }
+    };
+
     public static ShopDatabase getDatabase(final Context context) {
         if(INSTANCE == null) {
             synchronized(ShopDatabase.class) {
                 if(INSTANCE == null) {
                     INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
-                            ShopDatabase.class, "shop_db").build();
+                            ShopDatabase.class, "shop_db")
+                            .addCallback(sRoomDatabaseCallback)
+                            .build();
                 }
             }
         }

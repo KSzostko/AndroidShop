6397ad06b30ed7d727f6acb8913517d31ea60bad
Kuba Szostko <kubaszostko@gmail.com>
2021-01-09 17:21:44 +0100

    connected MainFragment with database

--- app/src/main/java/com/example/ecommerce/MainFragment.java
+++ app/src/main/java/com/example/ecommerce/MainFragment.java
@@ -8,20 +8,34 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.RatingBar;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
+import com.example.ecommerce.database.Product;
+import com.example.ecommerce.database.ShopViewModel;
 import com.squareup.picasso.Picasso;
 
+import java.util.List;
+
 public class MainFragment extends Fragment {
+    ShopViewModel shopViewModel;
+    List<Product> productsList;
+
     private CardView recommendedCard;
+    private TextView recommendedName;
+    private TextView recommendedPrice;
     private ImageView recommendedImageView;
     private RatingBar recommendedRating;
 
     private CardView bestsellerCard;
+    private TextView bestsellerName;
+    private TextView bestsellerPrice;
     private ImageView bestsellerImageView;
     private RatingBar bestsellserRating;
 
@@ -39,46 +53,77 @@ public class MainFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_main, container, false);
 
         recommendedCard = view.findViewById(R.id.recommended_card);
+        recommendedName = view.findViewById(R.id.recommended_name);
+        recommendedPrice = view.findViewById(R.id.recommended_price);
         recommendedImageView = view.findViewById(R.id.recommended_image);
         recommendedRating = view.findViewById(R.id.recommended_rating);
 
         bestsellerCard = view.findViewById(R.id.bestseller_card);
+        bestsellerName = view.findViewById(R.id.bestseller_name);
+        bestsellerPrice = view.findViewById(R.id.bestseller_price);
         bestsellerImageView = view.findViewById(R.id.bestseller_image);
         bestsellserRating = view.findViewById(R.id.bestseller_rating);
 
         recommendedRating.setRating(5);
 
-        recommendedCard.setOnClickListener(new View.OnClickListener() {
+        shopViewModel = new ViewModelProvider(this).get(ShopViewModel.class);
+        shopViewModel.findAllProducts().observe(getViewLifecycleOwner(), new Observer<List<Product>>() {
             @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getActivity(), ProductDetailsActivity.class);
+            public void onChanged(List<Product> products) {
+                productsList = products;
 
-                // TODO: add specific product data
-                startActivity(intent);
+                recommendedName.setText(products.get(0).getName());
+                recommendedPrice.setText(String.valueOf(products.get(0).getPrice()));
+                Picasso.get()
+                        .load(products.get(0).getImage())
+                        .into(recommendedImageView);
+
+                bestsellerName.setText(products.get(1).getName());
+                bestsellerPrice.setText(String.valueOf(products.get(1).getPrice()));
+                Picasso.get()
+                        .load(products.get(1).getImage())
+                        .into(bestsellerImageView);
+
+                recommendedCard.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(getActivity(), ProductDetailsActivity.class);
+                        intent.putExtra(ProductAdapter.PRODUCT_ID, products.get(0).getId());
+
+                        startActivity(intent);
+                    }
+                });
+
+                bestsellerCard.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(getActivity(), ProductDetailsActivity.class);
+                        intent.putExtra(ProductAdapter.PRODUCT_ID, products.get(1).getId());
+
+                        startActivity(intent);
+                    }
+                });
+
+                setRating(products.get(0), 0);
+                setRating(products.get(1), 1);
             }
         });
-
-        bestsellerCard.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getActivity(), ProductDetailsActivity.class);
-
-                // TODO: add specific product data
-                startActivity(intent);
-            }
-        });
-
-        String testImgUrl = "https://www.nvidia.com/content/dam/en-zz/Solutions/geforce/ampere/rtx-3090/geforce-rtx-3090-shop-630-d@2x.png";
-        // loading img
-        Picasso.get()
-                .load(testImgUrl)
-                .into(recommendedImageView);
-
-        bestsellserRating.setRating(4);
-        Picasso.get()
-                .load(testImgUrl)
-                .into(bestsellerImageView);
 
         return view;
+    }
+
+    private void setRating(Product product, int n) {
+        shopViewModel.findProductScore(product.getId()).observe(getViewLifecycleOwner(), new Observer<Float>() {
+            @Override
+            public void onChanged(Float aFloat) {
+                if(n == 0) {
+                    if(aFloat == null) recommendedRating.setRating(0);
+                    else recommendedRating.setRating(aFloat);
+                } else {
+                    if(aFloat == null) bestsellserRating.setRating(0);
+                    else bestsellserRating.setRating(aFloat);
+                }
+            }
+        });
     }
 }

5406365b6602f18930678689432729aee6a98959
Kuba Szostko <kubaszostko@gmail.com>
2021-01-17 10:14:16 +0100

    extracted method for populating products

--- app/src/main/java/com/example/ecommerce/database/ShopDatabase.java
+++ app/src/main/java/com/example/ecommerce/database/ShopDatabase.java
@@ -29,30 +29,15 @@ public abstract class ShopDatabase extends RoomDatabase {
         public void onOpen(@NonNull SupportSQLiteDatabase db) {
             super.onOpen(db);
             databaseWriteExecutor.execute(() -> {
-                ProductDao dao = INSTANCE.productDao();
-                // deleteAll probably should be removed later
-                dao.deleteAll();
+                populateProducts();
 
-                // TODO: generate separate method for this
-                Product product1 = new Product("MSI GeForce RTX 3090 GAMING X TRIO 24GB GDDR6X", 8080.00,
-                        "https://www.nvidia.com/content/dam/en-zz/Solutions/geforce/ampere/rtx-3090/geforce-rtx-3090-shop-300-t.png",
-                        "The latest iteration of MSI’s iconic GAMING series once again brings performance, low-noise efficiency, and aesthetics that hardcore" +
-                                " gamers have come to recognize and trust. " + "Now you too can enjoy all your favorite games with a powerful graphics card " +
-                                "that stays cool and silent. Just the way you like it.");
-                Product product2 = new Product("Huawei Matebook X Pro", 6999.00,
-                        "https://image.ceneostatic.pl/data/products/93456841/i-huawei-matebook-x-pro-2020-13-9-i7-16gb-1tb-win10-53011agg.jpg",
-                        "With an industry-leading processor and 3K FullView Display, HUAWEI MateBook X Pro takes your experience beyond the extraordinary. ");
-
-                dao.insert(product1);
-                dao.insert(product2);
-
-                OrderItemDao orderItemDao = INSTANCE.orderItemDao();
-                OrderDao orderDao = INSTANCE.orderDao();
-                ReviewDao reviewDao = INSTANCE.reviewDao();
+//                OrderItemDao orderItemDao = INSTANCE.orderItemDao();
+//                OrderDao orderDao = INSTANCE.orderDao();
+//                ReviewDao reviewDao = INSTANCE.reviewDao();
 //
-                orderItemDao.deleteAll();
-                orderDao.deleteAll();
-                reviewDao.deleteAll();
+//                orderItemDao.deleteAll();
+//                orderDao.deleteAll();
+//                reviewDao.deleteAll();
             });
         }
     };
@@ -71,5 +56,48 @@ public abstract class ShopDatabase extends RoomDatabase {
         }
 
         return INSTANCE;
+    }
+
+    private static void populateProducts() {
+        ProductDao dao = INSTANCE.productDao();
+
+        dao.deleteAll();
+
+        Product product1 = new Product("MSI GeForce RTX 3090 GAMING X TRIO 24GB GDDR6X", 8080.00,
+                "https://www.nvidia.com/content/dam/en-zz/Solutions/geforce/ampere/rtx-3090/geforce-rtx-3090-shop-300-t.png",
+                "The latest iteration of MSI’s iconic GAMING series once again brings performance, low-noise efficiency, and aesthetics that hardcore" +
+                        " gamers have come to recognize and trust. " + "Now you too can enjoy all your favorite games with a powerful graphics card " +
+                        "that stays cool and silent. Just the way you like it.");
+
+        Product product2 = new Product("Huawei Matebook X Pro", 6999.00,
+                "https://image.ceneostatic.pl/data/products/93456841/i-huawei-matebook-x-pro-2020-13-9-i7-16gb-1tb-win10-53011agg.jpg",
+                "With an industry-leading processor and 3K FullView Display, HUAWEI MateBook X Pro takes your experience beyond the extraordinary. ");
+
+        Product product3 = new Product("Lenovo Legion Y540-15", 5499.00,
+                "https://cdn.x-kom.pl/i/setup/images/prod/big/product-new-big,,2018/7/pr_2018_7_30_22_9_28_870_04.jpg",
+                "Ten 15,6-calowy laptop zapewnia dokładnie to, czego potrzebujesz do jeszcze bardziej wciągającej i płynniejszej gry. Najpotężniejsza " +
+                        "generacja mobilnych procesorów Intel Core i7 9-tej generacji to gwarancja najwyższej wydajności. Wyposażony w mocny układ graficzny "+
+                        "Nvidia GTX1660Ti pozwoli Ci grać w najnowsze gry. Zdominuj pole bitwy, zamów swój egzemplarz już dziś");
+
+        Product product4 = new Product("OnePlus 7T Pro", 2799.00,
+                "https://cdn.x-kom.pl/i/setup/images/prod/big/product-new-big,,2020/8/pr_2020_8_12_7_26_38_379_00.jpg",
+                "Poznaj OnePlus 7T Pro 8/256GB Haze Blue. Weź go do ręki i poczuj niedoścignioną moc potężnego procesora Qualcomm® Snapdragon™ 855+w grach " +
+                        "oraz multimediach. Odkryj także niesamowite wrażenia wizualne w projekcjach QHD+ na ekranie AMOLED 6,67” oraz inteligencję " +
+                        "potrójnego aparatu 48 Mpix gwarantującego ujęcia, które zapierają dech w piersiach. Masz przed sobą sprzęt, w którym nic nie " +
+                        "znalazło się przypadkowo. Tutaj wszystko jest Pro.");
+
+        Product product5 = new Product("IPhone 12 Pro", 5699.00,
+                "https://www.mediaexpert.pl/media/cache/gallery/product/1/633/956/83/7ekax059/images/26/2607281/Smartfon-APPLE-iPhone-12-Pro-Max-5G" +
+                        "-Pacyficzny-1.jpg",
+                "Apple iPhone 12 Pro 128GB Gold 5G to smartfon premium, który spełni najwyższe wymagania. Napędzany przez najsilniejszy na rynku procesor A14" +
+                        " Bionic zapewnia wielozadaniowość i pracę tak szybką, jak nigdy dotąd. Profesjonalny potrójny aparat ze skanerem LIDAR, wykona " +
+                        "najwyższej jakości zdjęcia także w nocy. Wszystko podziwiać będziesz na wysokiej jakości wyświetlaczu Super Retina XDR z matrycą " +
+                        "OLED.");
+
+        dao.insert(product1);
+        dao.insert(product2);
+        dao.insert(product3);
+        dao.insert(product4);
+        dao.insert(product5);
     }
 }
